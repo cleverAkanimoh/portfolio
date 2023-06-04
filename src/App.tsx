f6bb5ed4dd89bae6c7a7f5abb9e52c97257245b5
= <=>
2023-06-04 10:20:06 +0100

    changed 3 files

--- src/App.tsx
+++ src/App.tsx
@@ -5,32 +5,11 @@ import {
   createRoutesFromElements
 } from "react-router-dom"
 
-import Layout from "./Components/Layout";
-
-import About from "./Pages/About";
-import NotFound from "./Pages/NotFound";
-import Projects, {loader as projectsLoader} from "./Pages/Projects";
-import Skills from "./Pages/Skills";
-import Home from "./Pages/Home";
-import Contact from "./Pages/Contact";
-import ErrorPage from "./Pages/Error";
+import routes from "./routes";
 
 const router = createHashRouter(
   createRoutesFromElements(
-    <Route path="portfolio" element={<Layout />}>
-      <Route index element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="skills" element={<Skills />} />
-      <Route
-        path="projects"
-        element={<Projects />}
-        errorElement={<ErrorPage />}
-        loader={projectsLoader}
-      />
-      <Route path="connect" element={<Contact />} />
-
-      <Route path="*" element={<NotFound />} />
-    </Route>
+    routes
   )
 )
 
---
+++ src/routes.tsx
@@ -0,0 +1,26 @@
+import { Route } from "react-router-dom";
+
+import Layout from "./Components/Layout";
+
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Home from "./Pages/Home";
+import NotFound from "./Pages/NotFound";
+import Projects, {loader as projectsLoader} from "./Pages/Projects";
+import Skills from "./Pages/Skills";
+import ErrorPage from "./Pages/Error";
+
+export default  <Route path="portfolio" element={<Layout />}>
+    <Route index element={<Home />} />
+    <Route path="about" element={<About />} />
+    <Route path="skills" element={<Skills />} />
+    <Route
+        path="projects"
+        element={<Projects />}
+        errorElement={<ErrorPage />}
+        loader={projectsLoader}
+    />
+    <Route path="connect" element={<Contact />} />
+
+    <Route path="*" element={<NotFound />} />
+</Route>
