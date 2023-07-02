65427a6a5d4757c6d6fd3b479c26bb13e5527ee3
= <=>
2023-07-02 03:42:44 +0100

    changed 3 files

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,21 +6,36 @@ import About from "./Pages/About";
 import Contact from "./Pages/Contact";
 import Home from "./Pages/Home";
 import NotFound from "./Pages/NotFound";
-import Projects, {loader as projectsLoader} from "./Pages/Projects";
+import Projects, { loader as projectsLoader } from "./Pages/Projects";
 import Skills from "./Pages/Skills";
 import ErrorPage from "./Pages/Error";
 
-export default  <Route path="portfolios" element={<Layout />}>
-    <Route index element={<Home />} />
-    <Route path="about" element={<About />} />
-    <Route path="skills" element={<Skills />} />
-    <Route
-        path="projects"
-        element={<Projects />}
-        errorElement={<ErrorPage />}
-        loader={projectsLoader}
-    />
-    <Route path="connect" element={<Contact />} />
+export default (
+    <>
+        <Route path="" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route 
+                path="about"
+                element={<About />}
+            />
+            
+            <Route 
+                path="skills"
+                element={<Skills />}
+            />
+            
+            <Route
+                path="projects"
+                element={<Projects />}
+                errorElement={<ErrorPage />}
+                loader={projectsLoader} />
+                
+            <Route
+                path="connect"
+                element={<Contact />}
+            />
 
-    <Route path="*" element={<NotFound />} />
-</Route>
+        </Route>
+        <Route path="*" element={<NotFound />} />
+    </>
+)
