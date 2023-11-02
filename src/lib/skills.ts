2c757ff6acb72e17172111b26bd7749dcc5cc1ad
= <=>
2023-11-02 15:24:27 +0100

    auto-commit

--- src/lib/skills.ts
+++ src/lib/skills.ts
@@ -1,5 +1,5 @@
 
-export const frontend = ['HTML', 'CSS', 'JQUERY', 'BOOTSTRAP', 'TAILWINDCSS', 'UIUX', 'REACT.JS', 'REDUX']
+export const frontend = ['HTML', 'CSS', 'JQUERY', 'BOOTSTRAP', 'TAILWINDCSS', 'UIUX', 'REACT.JS', "NEXTJS"]
 
 export const backend = ['EXPRESS.JS', 'DJANGO', 'restAPI']
 
@@ -7,7 +7,7 @@ export const languages = ['PYTHON', 'JAVASCRIPT', 'TYPESCRIPT']
 
 export const libraries = ['JQUERY', 'BOOTSTRAP', 'REACT-ROUTER', 'REACT-ICONS', 'REDUX']
 
-export const frameworks = ['FIREBASE', 'DJANGO', 'REACTJS', 'TAILWINDCSS']
+export const frameworks = ['FIREBASE', 'DJANGO', 'REACTJS', 'TAILWINDCSS', "NEXTJS"]
 
 export const dataAnalysis = ['SQL', 'BIG_QUERY', 'SPREADSHEETS', 'DOCS', 'POWERPOINT PRESENTATION']
 
